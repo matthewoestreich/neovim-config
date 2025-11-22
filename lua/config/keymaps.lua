9b4fcbed5338070ded2e88414a73ac8c7ee405c5
Matt Oestreich <matthewpoestreich@gmail.com>
2025-11-22 05:40:28 -0600

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,14 +7,6 @@
 -- Jump to Nvim-Tree window
 vim.api.nvim_set_keymap("n", "<Leader>t", "<C-w>h", { noremap = true, silent = true })
 
--- jump to the currently active buffer from Neo-tree
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>o", -- or whatever key you like
-  "<cmd>Neotree reveal<CR>",
-  { noremap = true, silent = true }
-)
-
 --Delete without cutting (does not add deleted text to buffer).
 vim.keymap.set("n", "<S-d>", '"_dd', { desc = "Delete line without affecting registers" })
 vim.keymap.set("v", "<S-d>", '"_d', { desc = "Delete selection without affecting registers" })
