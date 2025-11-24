50b284d391bbb8f0a2299d78cfa9c2a334d05022
Matt Oestreich <matthewpoestreich@gmail.com>
2025-11-23 20:42:09 -0600

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,4 @@ vim.hl.priorities.semantic_tokens = 50 -- Default is often 100
 
 -- Press `jj` to go back to Normal mode.
 vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true })
+vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true })
