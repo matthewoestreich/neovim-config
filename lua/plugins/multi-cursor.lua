bd5f23a01f9681763cf2fd7d6c1e41ad597b14e1
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-06 12:17:59 -0600

    update

---
+++ lua/plugins/multi-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    init = function()
+      -- Option + Shift + Up/Down (⌥+⇧)
+      vim.keymap.set("n", "<M-S-Up>", "<Plug>(VM-Add-Cursor-Up)", { noremap = true, silent = true })
+      vim.keymap.set("n", "<M-S-Down>", "<Plug>(VM-Add-Cursor-Down)", { noremap = true, silent = true })
+    end,
+  },
+}
