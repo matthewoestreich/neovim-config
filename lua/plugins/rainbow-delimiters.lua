00cb262e7b65493c854925181fe0642fd6c4194d
Matt Oestreich <matthewpoestreich@gmail.com>
2025-12-12 21:51:04 -0600

    update

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -2,6 +2,7 @@ return {
   {
     "HiPhish/rainbow-delimiters.nvim",
     config = function()
+      ---@diagnostic disable-next-line: unused-local
       local rainbow = require("rainbow-delimiters")
 
       vim.g.rainbow_delimiters = {
