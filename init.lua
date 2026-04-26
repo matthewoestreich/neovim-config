f3ac2bafce5cdf0550160c881b31e043d91acf23
Matt Oestreich <matthewpoestreich@gmail.com>
2026-04-25 20:15:36 -0500

    colorscheme

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Could set colorscheme here.....
+-- vim.cmd.colorscheme("github_dark_high_contrast")
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -34,7 +34,22 @@ local pitchblack = {
   opts = {},
 }
 
+local gh_dark_high_contrast = {
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      -- ...
+    })
+
+    vim.cmd("colorscheme github_dark_high_contrast")
+  end,
+}
+
 return {
   --kanigawa,
-  pitchblack,
+  --pitchblack,
+  gh_dark_high_contrast,
 }
